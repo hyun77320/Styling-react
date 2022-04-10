c40463b623e42c29a140f4269129166e38c59c29
iqamac <hyun77320@naver.com>
2022-04-10 19:14:32 +0900

    2022.04.10.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { css, ThemeProvider } from 'styled-components'; // eslint-disable-line no-unused-vars
 import Button from './components/Button';
+import Dialog from './components/Dialog';
 
 const AppBlock = styled.div`
   width: 512px;
@@ -27,28 +28,42 @@ function App() {
         }
       }}
     >
-      <AppBlock>
-        <ButtonGroup>
-          <Button size='large'>BUTTON</Button>
-          <Button>BUTTON</Button>
-          <Button size='small'>BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color='gray' size='large'>BUTTON</Button>
-          <Button color='gray'>BUTTON</Button>
-          <Button color='gray' size='small'>BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color='pink' size='large'>BUTTON</Button>
-          <Button color='pink'>BUTTON</Button>
-          <Button color='pink' size='small'>BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size='large' outline>BUTTON</Button>
-          <Button color='gray' outline>BUTTON</Button>
-          <Button color='pink' size='small' outline>BUTTON</Button>
-        </ButtonGroup>
-      </AppBlock>
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button size='large'>BUTTON</Button>
+            <Button>BUTTON</Button>
+            <Button size='small'>BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color='gray' size='large'>BUTTON</Button>
+            <Button color='gray'>BUTTON</Button>
+            <Button color='gray' size='small'>BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color='pink' size='large'>BUTTON</Button>
+            <Button color='pink'>BUTTON</Button>
+            <Button color='pink' size='small'>BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size='large' outline>BUTTON</Button>
+            <Button color='gray' outline>BUTTON</Button>
+            <Button color='pink' size='small' outline>BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button fullWidth>BUTTON</Button>
+            <Button color='gray' fullWidth>BUTTON</Button>
+            <Button color='pink' fullWidth>BUTTON</Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title='정말로 삭제하시겠습니까?'
+          confirmText='삭제'
+          cancelText='취소'
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,71 +1,187 @@
-import React from "react";
-import styled, {css} from 'styled-components';
-import { darken,lighten } from "polished";
+    // import React from "react";
+    // import styled, {css} from 'styled-components';
+    // import { darken, lighten } from "polished";
 
-const colorStyles = css`
+    // const colorStyles = css`
+    //     ${({ theme, color }) => {
+    //         const selected = theme.palette[color];
+    //         return css`
+    //             background: ${selected};
+    //             &:hover {
+    //                 background: ${lighten(0.1, selected)};
+    //             }
+    //             &:active {
+    //                 background: ${darken(0.1, selected)};
+    //             }
+    //             ${props =>
+    //                 props.outline &&
+    //                 css`
+    //                     color: $(selected);
+    //                     background: none;
+    //                     border: 1px solid ${selected};
+    //                     $:hover {
+    //                         background: ${selected};
+    //                         color: white;
+    //                     }
+    //                 `
+    //             }
+    //         `;
+    //     }}
+    // `;
+
+    // const sizes = {
+    //     large: {
+    //         height: '3rem',
+    //         fontsize: '1.25rem'
+    //     },
+    //     medium: {
+    //         height: '2.25rem',
+    //         fontsize: '1rem'
+    //     },
+    //     samll: {
+    //         height: '1.75rem',
+    //         fontsize: '0.85rem'
+    //     }
+    // };
+
+    // const sizeStyles = css`
+    //     ${({ size }) => css`
+    //         height: ${sizes[size].height};
+    //         font-size: ${sizes[size].fontSize};
+    //     `}
+    // `;
+
+    // const fullWidthStyle = css`
+    //     ${props =>
+    //         props.fullWidth &&
+    //         css`
+    //             width: 100%;
+    //             justify-content: center;
+    //             & + & {
+    //                 margin-left: 0;
+    //                 margin-top: 1rem;
+    //             }
+    //         `
+    //     }
+    // `;
+
+    // const StyledButton = styled.button`
+    //     /* 공통 스타일 */
+    //     display: inline-flex;
+    //     outline: none;
+    //     border: none;
+    //     border-radius: 4px;
+    //     color: white;
+    //     font-weight: bold;
+    //     cursor: pointer;
+    //     padding-left: 1rem;
+    //     padding-right: 1rem;
+    //     align-items: center;
+
+    //     /* 크기 */
+    //     ${sizeStyles}
+
+    //     /* 색상 */
+    //     ${colorStyles}
+
+    //     /* 기타 */
+    //     & + & {
+    //         margin-left: 1rem;
+    //     }
+
+    //     ${fullWidthStyle}
+    // `;
+
+    // function Button({ children, color, size, outline, fullWidth, ...rest }) {
+    //     return (
+    //         <StyledButton 
+    //             color={color} 
+    //             size={size} 
+    //             outline={outline}
+    //             fullWidth={fullWidth}
+    //             {...rest}
+    //         >
+    //             {children}
+    //         </ StyledButton>
+    //     );
+    // }
+
+    // Button.defaultProps = {
+    //     color: 'blue',
+    //     size: 'medium'
+    // };
+
+    // export default Button;
+
+
+    import React from 'react';
+    import styled, { css } from 'styled-components';
+    import { darken, lighten } from 'polished';
+
+    const colorStyles = css`
     ${({ theme, color }) => {
         const selected = theme.palette[color];
         return css`
-            background: ${selected};
+        background: ${selected};
+        &:hover {
+            background: ${lighten(0.1, selected)};
+        }
+        &:active {
+            background: ${darken(0.1, selected)};
+        }
+        ${props =>
+            props.outline &&
+            css`
+            color: ${selected};
+            background: none;
+            border: 1px solid ${selected};
             &:hover {
-                background: ${lighten(0.1, selected)};
+                background: ${selected};
+                color: white;
             }
-            &:active {
-                background: ${darken(0.1, selected)};
-            }
-            ${props =>
-                props.outline &&
-                css`
-                    color: $(selected);
-                    background: none;
-                    border: 1px solid ${selected};
-                    $:hover {
-                        background: ${selected};
-                        color: white;
-                    }
-                `
-            }
+            `}
         `;
     }}
-`;
+    `;
 
-const sizes = {
+    const sizes = {
     large: {
         height: '3rem',
-        fontsize: '1.25rem'
+        fontSize: '1.25rem'
     },
     medium: {
         height: '2.25rem',
-        fontsize: '1rem'
+        fontSize: '1rem'
     },
-    samll: {
+    small: {
         height: '1.75rem',
-        fontsize: '0.85rem'
+        fontSize: '0.875rem'
     }
-};
+    };
 
-const sizeStyles = css`
-    ${({ size}) => css`
+    const sizeStyles = css`
+    ${({ size }) => css`
         height: ${sizes[size].height};
-        font-size: ${sizes[size].fontsize};
+        font-size: ${sizes[size].fontSize};
     `}
-`;
+    `;
 
-const fullWidthStyle = css`
+    const fullWidthStyle = css`
     ${props =>
         props.fullWidth &&
         css`
-            width: 100%;
-            justify-content: center;
-            & + & {
-                margin-left: 0;
-                margin-top: 1rem;
-            }
-        `
-    }
-`
+        width: 100%;
+        height: 3rem;
+        font-size: 1.25rem;
+        justify-content: center;
+        & + & {
+            margin-top: 1rem;
+            margin-left: 0;
+        }
+        `}
+    `;
 
-const StyledButton = styled.button`
+    const StyledButton = styled.button`
     /* 공통 스타일 */
     display: inline-flex;
     outline: none;
@@ -77,6 +193,8 @@ const StyledButton = styled.button`
     padding-left: 1rem;
     padding-right: 1rem;
     align-items: center;
+    margin-right: 1rem;
+    margin-top: 16px;
 
     /* 크기 */
     ${sizeStyles}
@@ -86,28 +204,29 @@ const StyledButton = styled.button`
 
     /* 기타 */
     & + & {
-        margin-left: 1rem;
+
     }
-`;
 
-function Button({ children, color, size, outline, fullWidth, ...rest }) {
+    ${fullWidthStyle}
+    `;
+
+    function Button({ children, color, size, outline, fullWidth, ...rest }) {
     return (
-        <StyledButton 
-            color={color} 
-            size={size} 
-            outline={outline}
-            fullWidth={fullWidth}
-            {...rest}
+        <StyledButton
+        color={color}
+        size={size}
+        outline={outline}
+        fullWidth={fullWidth}
+        {...rest}
         >
-            {children}
-        </ StyledButton>
+        {children}
+        </StyledButton>
     );
-}
+    }
 
-Button.defaultProps = {
+    Button.defaultProps = {
     color: 'blue',
     size: 'medium'
-};
+    };
 
-export default Button;
-
+    export default Button;
